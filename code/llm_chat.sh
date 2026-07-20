d02fb246c8f76197ec28c6dc5ee74b5ee90a242c
Richard Kuo <rkuo2000@gmail.com>
2026-07-21 00:38:01 +0800

    Delete code/llm_chat.sh

--- code/llm_chat.sh
+++
@@ -1,4 +0,0 @@
-curl -s http://localhost:11434/v1/chat/completions \
-  -H "Content-Type: application/json" \
-  -d '{"model":"gemma4-turbo-128k:e2b","messages":[{"role":"user","content":"Hi!"}],"max_tokens":256}' \
-  | python3 -m json.tool
