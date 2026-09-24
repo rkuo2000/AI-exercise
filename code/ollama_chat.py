e2ff5680d7c389cc7bb2170fa5314efee1a0f953
Richard Kuo <rkuo2000@gmail.com>
2026-09-25 01:58:27 +0800

    Update OpenAI base URL in ollama_chat.py

--- code/ollama_chat.py
+++ code/ollama_chat.py
@@ -2,7 +2,7 @@
 from openai import OpenAI
 
 client = OpenAI(
-#        base_url='http://192.168.0.22:11434/v1',
+#        base_url='http://192.168.0.13:11434/v1',
         base_url='http://localhost:11434/v1',
         api_key="ollama"
 )
